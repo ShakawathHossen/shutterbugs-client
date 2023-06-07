813c49acc7d8fd31c3790860683187a88cc0041b
ShakawathHossen <shakawath15-2483@diu.edu.bd>
2023-06-07 06:00:35 +0600

    Tailwind and daisy ui install

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            This is home
+              <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button className="btn btn-secondary">Button</button>
+<button className="btn btn-accent">Button</button>
+<button className="btn btn-ghost">Button</button>
         </div>
     );
 };
