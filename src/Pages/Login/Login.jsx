987ead651f28ea81b19e7a782dca49a6370bf620
ShakawathHossen <shakawath15-2483@diu.edu.bd>
2023-06-09 23:06:25 +0600

    email password login successfull

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,138 +1,117 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Login.css'
-import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
+import './Login.css';
+import { useForm } from 'react-hook-form';
 import { AuthContext } from '../../Providers/AuthProviders';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import app from '../../firebase/firebase.config';
 
-
-
 const Login = () => {
-    const {signIn}= useContext(AuthContext)
-    const Auth= getAuth(app);
-    const googleProvider= new GoogleAuthProvider();
+  const { signIn } = useContext(AuthContext);
+  const Auth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
 
+  const [showPassword, setShowPassword] = useState(false); // State for password visibility
 
+  // Function to handle email and password sign in
+  const handleEmailPasswordSignIn = data => {
+    const { email, password } = data;
+    signInWithEmailAndPassword(Auth, email, password)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+        // Redirect to desired page after successful login
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
+  // Function to handle Google sign in
+  const handleGoogleSignIn = () => {
+    signInWithPopup(Auth, googleProvider)
+      .then(result => {
+        const loggedInUser = result.data;
+        console.log(loggedInUser);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
-
-
-
-      // google sign in 
-
-      const handleGoogleSignIn=()=>{
-        signInWithPopup(Auth,googleProvider)
-        .then(result=>{
-            const loggedInUser=result.data;
-
-
-        })
-        .catch(err=>{console.log(err);});
-    }
-
-    // google sign in 
-
-
-
-
-
-
-
-
-
-
-
-
-    const handleLogin = event => {
-        event.preventDefault();
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email, password);
-        if (password.length < 6) {
-           alert('password must be 6 character long')
-        }
-        // email password sign in 
-        // signIn(email, password)
-        //     .then(result => {
-        //         const user = result.user;
-        //         console.log(user);
-        //         nevigate(from,{replace:true})
-        //         Swal.fire({
-        //             title: 'login Successfull',
-        //             text: 'Continue your browsing',
-        //             icon: 'success',
-        //             confirmButtonText: 'Thanks'
-        //           })
-        //     })
-        //     .catch((error) => {
-        //         Swal.fire(
-        //             'User and password not matched',
-        //           )
-        //     });
-        
-    }
-
-
-
-
-
-    return (
-        <div className='md:py-6 background'>
-            <div className="grid md:grid-cols-3 py-16 justify-center align-content-center justify-items-center">
-                <div></div>
-
-
-
-                <div className='shadow-xl shadow-slate-800 rounded-3xl'>
-                    <form onSubmit={handleLogin} className="card-body">
-                        <div className="form-control ">
-                            <label className="label">
-
-                            </label>
-
-                            <input type="email" name='email' placeholder="Your Email" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-
-                            </label>
-
-                            <input type="password" name='password' placeholder="Password" className="input input-bordered" required />
-
-                            <label className="label">
-                                <small><input type="checkbox" name="" id="" /> show Password</small>
-                            </label>
-                        </div>
-                        
-                        <div className="mt-6 form-control">
-                            <button className="border customButton">Login</button>
-                        </div>
-                        <div className='text-center  mt-6'>
-                            <p className='text-lg  divider '>Or Connect With</p>
-                            <div className='my-4'>
-                                <button onClick={handleGoogleSignIn} className='px-4'>
-                                    <img className='w-10' src="https://i.ibb.co/ftwyb00/Google-G-Logo-svg.png" alt="" />
-                                </button>
-                                <button className='px-4'>
-                                    <img className='w-10' src="https://i.ibb.co/VxKN3Mg/github.png" alt="" />
-
-                                </button>
-                            </div>
-                            <div>
-                                <p className='text-sm'>New to <span className='font-semibold text-[#283149]'>SutterBugs</span> ?<Link to="/registration"><button className="btn btn-active btn-link normal-case text-sm text-sky-700 ">Registration Here</button>
-                                </Link></p>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <div></div>
+  return (
+    <div className="md:py-6 background">
+      <div className="grid md:grid-cols-3 py-16 justify-center align-content-center justify-items-center">
+        <div></div>
+        <div className="shadow-xl shadow-slate-800 rounded-3xl">
+          <form onSubmit={handleSubmit(handleEmailPasswordSignIn)} className="card-body">
+            <div className="form-control">
+              <label className="label">Email</label>
+              <input
+                type="email"
+                {...register('email', { required: true })}
+                name="email"
+                placeholder="Your Email"
+                className="input input-bordered"
+                required
+              />
+              {errors.email && <span className="error-message">This field is required</span>}
             </div>
+            <div className="form-control">
+              <label className="label">Password</label>
+              <input
+                type={showPassword ? 'text' : 'password'}
+                {...register('password', { required: true })}
+                name="password"
+                placeholder="Password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <small>
+                  <input type="checkbox" checked={showPassword} onChange={togglePasswordVisibility} /> Show Password
+                </small>
+              </label>
+              {errors.password && <span className="error-message">This field is required</span>}
+            </div>
+            <div className="mt-6 form-control">
+              <button className="border customButton">Login</button>
+            </div>
+            <div className="text-center mt-6">
+              <p className="text-lg divider">Or Connect With</p>
+              <div className="my-4">
+                <button onClick={handleGoogleSignIn} className="px-4">
+                  <img className="w-10" src="https://i.ibb.co/ftwyb00/Google-G-Logo-svg.png" alt="" />
+                </button>
+                <button className="px-4">
+                  <img className="w-10" src="https://i.ibb.co/VxKN3Mg/github.png" alt="" />
+                </button>
+              </div>
+              <div>
+                <p className="text-sm">
+                  New to <span className="font-semibold text-[#283149]">SutterBugs</span>?
+                  <Link to="/registration">
+                    <button className="btn btn-active btn-link normal-case text-sm text-sky-700">Registration Here</button>
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </form>
         </div>
-    );
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,109 +1,145 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../Providers/AuthProviders';
 
 const Navbar = () => {
-    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const { user,logOut } = useContext(AuthContext)
 
-    const toggleMobileMenu = () => {
-      setIsMobileMenuOpen(!isMobileMenuOpen);
-    };
-  
-  
-    const navOptions = <>
-  
-      <li>
-        <Link to="/" className="text-white hover:text-gray-300">
-          Home
-        </Link>
-      </li>
-    
-      <li>
-        <Link to="/instructors" className="text-white hover:text-gray-300">
+  const handleLogOut=()=>{
+    logOut()
+    .then(result=>{
+      console.log(result);
+
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+  }
+
+
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+
+  const navOptions = <>
+
+    <li>
+      <Link to="/" className="text-white hover:text-gray-300">
+        Home
+      </Link>
+    </li>
+
+    <li>
+      <Link to="/instructors" className="text-white hover:text-gray-300">
         Instructors
-        </Link>
-      </li>
-      <li>
-        <Link to="/classes" className="text-white hover:text-gray-300">
-          Classes
-        </Link>
-      </li>
-      <li>
-        <Link to="/dashboard" className="text-white hover:text-gray-300">
+      </Link>
+    </li>
+    <li>
+      <Link to="/classes" className="text-white hover:text-gray-300">
+        Classes
+      </Link>
+    </li>
+    <li>
+      <Link to="/dashboard" className="text-white hover:text-gray-300">
         Dashboard
-        </Link>
-      </li>
-      <li>
-        <Link to="/login" className="text-white hover:text-gray-300">
-          Sign In
-        </Link>
-      </li>
-      <li>
-        <Link to="/profile" className="text-white hover:text-gray-300">
-          <img
-            src=""
-            alt="Profile"
-            className="w-8 h-8 rounded-full"
-          />
-        </Link>
-      </li>
-    </>
-  
-    return (
-        <div>
-            <nav className="flex items-center justify-between px-4 py-3 md:bg-gray-900 md:opacity-70 md:fixed md:z-10 w-full bg-gray-900 ">
-      {/* Logo */}
-      <div className="flex items-center">
-        <img src="" alt="Logo" className="w-8 h-8" />
-        <span className="text-white ml-2 text-lg font-semibold">Your Logo</span>
-      </div>
+      </Link>
+    </li>
+    <li>
+      <Link  className="text-white hover:text-gray-300">
+        <img
+          src=""
+          alt="Profile"
+          className="w-8 h-8 rounded-full"
+        />
+      </Link>
+    </li>
 
-      {/* Mobile Menu Toggle */}
-      <div className="md:hidden">
-        <button
-          onClick={toggleMobileMenu}
-          className="text-white focus:outline-none"
-        >
-          <svg
-            className="w-6 h-6 fill-current"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+
+
+    {
+      user ?
+        <>
+          <li>
+            <Link onClick={handleLogOut} className="text-white hover:text-gray-300">
+              Logout
+            </Link>
+          </li>
+        </> :
+        <>
+          <li>
+            <Link to="/login" className="text-white hover:text-gray-300">
+              Log In
+            </Link>
+          </li>
+        </>
+    }
+
+
+
+
+
+
+  </>
+
+  return (
+    <div>
+      <nav className="flex items-center justify-between px-4 py-3 md:bg-gray-900 md:opacity-70 md:fixed md:z-10 w-full bg-gray-900 ">
+        {/* Logo */}
+        <div className="flex items-center">
+          <img src="" alt="Logo" className="w-8 h-8" />
+          <span className="text-white ml-2 text-lg font-semibold">Your Logo</span>
+        </div>
+
+        {/* Mobile Menu Toggle */}
+        <div className="md:hidden">
+          <button
+            onClick={toggleMobileMenu}
+            className="text-white focus:outline-none"
           >
-            {isMobileMenuOpen ? (
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M20.293 4.293a1 1 0 0 0-1.414 0L12 10.586 5.121 3.707a1 1 0 0 0-1.414 1.414L10.586 12l-6.879 6.879a1 1 0 1 0 1.414 1.414L12 13.414l6.879 6.879a1 1 0 0 0 1.414-1.414L13.414 12l6.879-6.879a1 1 0 0 0 0-1.414z"
-              />
-            ) : (
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M4 6a1 1 0 1 1 0-2h16a1 1 0 0 1 0 2H4zm0 5a1 1 0 1 1 0-2h16a1 1 0 0 1 0 2H4zm0 5a1 1 0 1 1 0-2h16a1 1 0 0 1 0 2H4z"
-              />
-            )}
-          </svg>
-        </button>
-      </div>
+            <svg
+              className="w-6 h-6 fill-current"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {isMobileMenuOpen ? (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M20.293 4.293a1 1 0 0 0-1.414 0L12 10.586 5.121 3.707a1 1 0 0 0-1.414 1.414L10.586 12l-6.879 6.879a1 1 0 1 0 1.414 1.414L12 13.414l6.879 6.879a1 1 0 0 0 1.414-1.414L13.414 12l6.879-6.879a1 1 0 0 0 0-1.414z"
+                />
+              ) : (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4 6a1 1 0 1 1 0-2h16a1 1 0 0 1 0 2H4zm0 5a1 1 0 1 1 0-2h16a1 1 0 0 1 0 2H4zm0 5a1 1 0 1 1 0-2h16a1 1 0 0 1 0 2H4z"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
 
-      {/* Desktop Menu */}
-      <div className="hidden md:flex md:items-center">
-        <ul className="flex items-center space-x-4">
-         {navOptions}
-        </ul>
-      </div>
-
-      {/* Mobile Menu */}
-      {isMobileMenuOpen && (
-        <div className="mt-4 md:hidden absolute z-20 right-0 top-10 w-1/2">
-          <ul className="bg-gray-900 py-2 px-4 space-y-2">
-          {navOptions}
+        {/* Desktop Menu */}
+        <div className="hidden md:flex md:items-center">
+          <ul className="flex items-center space-x-4">
+            {navOptions}
           </ul>
         </div>
-      )}
-    </nav>
-            
-        </div>
-    );
+
+        {/* Mobile Menu */}
+        {isMobileMenuOpen && (
+          <div className="mt-4 md:hidden absolute z-20 right-0 top-10 w-1/2">
+            <ul className="bg-gray-900 py-2 px-4 space-y-2">
+              {navOptions}
+            </ul>
+          </div>
+        )}
+      </nav>
+
+    </div>
+  );
 };
 
 export default Navbar;
